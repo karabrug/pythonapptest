cb5849c7ee2f1069cfd16aa5712aa91b76068f3b
karabrug <kara.brugman@gmail.com>
2018-09-19 15:25:44 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cpxrhyotherm',
+      version='0.1',
+      description='Low-Aluminum Clinopyroxene-Liquid Geothermometer for High-Silica Magmatic Systems',
+      url='http://github.com/karabrug/cpxrhyotherm',
+      author='Karalee K. Brugman',
+      author_email='kara.brugman@gmail.com',
+      license='GNU GPL v.3',
+      packages=['cpxrhyotherm'],
+      zip_safe=False)
